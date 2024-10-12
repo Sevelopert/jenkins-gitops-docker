8660e7fd86531efc7a0d07b5f0c22c0ff68427a2
Sevelopert <148232958+Sevelopert@users.noreply.github.com>
2024-10-12 08:47:42 +0300

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello Sam'
+    return 'Hello Seva, great job in building your first CI/CD Pipeline, keep it up.'
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
